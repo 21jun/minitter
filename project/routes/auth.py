e2261208de2eb27434cebfacfc9d0516d8210b51
21jun <21jun7654@gmail.com>
2020-08-14 20:03:06 +0900

    sign-up

--- project/routes/auth.py
+++ project/routes/auth.py
@@ -9,24 +9,19 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 @auth_bp.route('/sign-up', methods=['POST'])
 def sign_up():
 
-    print(request.__dict__)
-
     new_user = request.get_json()
     print(new_user)
 
-    id = new_user.get("id", None)
-    pw = new_user.get("password", None)
-    name = new_user.get("name", "default_user_name")
-    email = new_user.get("email", "defualt_user_email")
-    profile = new_user.get("profile", "")
-    created_at = datetime.now()
+    new_user = {
+        "hashed_password": new_user.get("password", None),
+        "name": new_user.get("name", "default_user_name"),
+        "email": new_user.get("email", "defualt_user_email"),
+        "profile": new_user.get("profile", ""),
+        "created_at": datetime.now()
+    }
 
     # TODO: hash password
-    user = User(id=id, hashed_password=pw, name=name, email=email,
-                profile=profile, created_at=created_at)
-
-    # user = User(**new_user)???
-    print(id, pw, profile)
+    user = User(**new_user)
 
     db.session.add(user)
     db.session.commit()
--- test/test_auth.py
+++ test/test_auth.py
@@ -2,7 +2,17 @@ import json
 
 
 def test_sign_up(app, client):
-    res = client.post('/auth/sign-up', data=json.dumps({"name": "Lee"}),
+    payload = {
+        "name": "Lee",
+        "password": "111",
+        "profile": "Hi There!"
+    }
+    res = client.post('/auth/sign-up', data=json.dumps(payload),
                       content_type='application/json')
     data = res.get_json()
+
     assert str(data['name']) == "Lee"
+
+
+def test_login(app, client):
+    pass
