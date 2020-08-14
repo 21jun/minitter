25530e5e54bcee10605f27d8811e6d170e7c4260
21jun <21jun7654@gmail.com>
2020-08-14 19:01:11 +0900

    auth

--- project/models/users.py
+++ project/models/users.py
@@ -1,14 +1,16 @@
 from . import db
 import datetime
-from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy import Column, DateTime, Integer, String
 
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, nullable=False,
-                   primary_key=True, autoincrement=True)
-    hashed_password = db.Column(db.String, nullable=False)
+    id = Column(Integer, nullable=False,
+                primary_key=True, autoincrement=True)
+    hashed_password = Column(String, nullable=False)
 
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
-    profile = db.Column(db.String, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    profile = Column(String, nullable=False)
+    created_at = Column(
+        DateTime, default=datetime.datetime.utcnow, nullable=False)
--- project/routes/auth.py
+++ project/routes/auth.py
@@ -1,12 +1,34 @@
+from datetime import datetime
 from flask import Blueprint, jsonify, request
-from random import randint
+from project.models.users import User
+from project.models import db
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @auth_bp.route('/sign-up', methods=['POST'])
 def sign_up():
+
+    print(request.__dict__)
+
     new_user = request.get_json()
     print(new_user)
-    new_user['id'] = randint(1, 1000)
+
+    id = new_user.get("id", None)
+    pw = new_user.get("password", None)
+    name = new_user.get("name", "default_user_name")
+    email = new_user.get("email", "defualt_user_email")
+    profile = new_user.get("profile", "")
+    created_at = datetime.now()
+
+    # TODO: hash password
+    user = User(id=id, hashed_password=pw, name=name, email=email,
+                profile=profile, created_at=created_at)
+
+    # user = User(**new_user)???
+    print(id, pw, profile)
+
+    db.session.add(user)
+    db.session.commit()
+
     return jsonify(new_user)
